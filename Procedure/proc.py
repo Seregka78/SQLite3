5a6489e8ff78e6bc05c7d176c8ef7f90acdf8666
Seredka78 <Seregka78@gmail.com>
2024-03-23 23:01:39 +0300

    Добавил корректировку столбцов в таблице

--- Procedure/proc.py
+++ Procedure/proc.py
@@ -1,22 +1,91 @@
 import sqlite3
-con=sqlite3.connect('sqlite3.db') #Создаем файл с базой данных
-cur = con.cursor()
-def ADD_info():
+from sqlite3 import Error
+# Строки – импорт sqlite3 и класса Error.
+
+
+def create_connection(path):
+    connect = None
+    try:
+        connect = sqlite3.connect(path) #Создаем файл с базой данных
+        print("Connection to SQLite DB successful")   #выводится состояние успешного подключения к базе данных.
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connect
+#перехватывает любое  исключение, которое может быть
+# получено, если методу.connect() не удастся установить соединение
+# В следующей строке-сообщение об ошибке
+
+def ADD_basa(connect):
+    cursor = connect.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS car (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            mode STRING,
+            model STRING,
+            name_id STRING) ''')
+    cursor.close()
+
+def ADD_info(connect):
+    cursor = connect.cursor()
     x = 0
-    i = int(input('Введите сколько моделей хотите добавить в базу данных'))
+    i = int(input('Введите сколько моделей хотите добавить в базу данных \n'))
 
     for x in range(i):
-        md = str(input('Укажите производителя'))
-        ml = str(input('Укажите модель'))
-        n = str(input('Укажите серийный номер'))
+        md = str(input('Укажите производителя\n'))
+        ml = str(input('Укажите модель\n'))
+        n = str(input('Укажите серийный номер\n'))
+        cursor.execute(" INSERT INTO car (mode, model, name_id) VALUES(?,?,?);", (md, ml, n))
         print(f'''добавили {x + 1}''')
         x += 1
-        cur.execute(" INSERT INTO car (mode, model, name_id) VALUES(?,?,?);", (md, ml, n))
         #
         # В документации написано, что метод принимает два аргумента - строку запроса и кортеж аргументов
         # cur.execute("INSERT INTO names VALUES(?, ?, ?);", (name, age, gender))
-        con.commit()
+    cursor.close()
 
-def All_print_consol():
-    res = cur.execute('SELECT * FROM car')                  #вывести в консоль все
+def All_print_consol(connect):
+    cursor = connect.cursor()
+    res = cursor.execute('SELECT * FROM car')                  #вывести в консоль все
     print('Вот что сейчас в базе данных', res.fetchall())
+    cursor.close()
+
+def ADD_colum(connect, name_colum):
+    cursor = connect.cursor()
+    cursor.execute(f'''ALTER TABLE car ADD COLUMN {name_colum}''')
+    print('добавили колонку', name_colum)
+    cursor.close()
+
+
+def corect_info(connect):
+    cursor = connect.cursor()
+    while True:
+        All_print_consol(connect)
+        id_info = int(input('Укажите ID строчки для  просмотра\n'))
+        prnt_name=cursor.execute('PRAGMA table_info(car)')  #Вывод имени столбцов
+        desc = prnt_name.fetchall()
+        names = [fields[1] for fields in desc]              #выбираем первые элементы из списка
+        print(names)
+        resultat=cursor.execute(f''' SELECT * FROM car where id={id_info}''')
+        print('Выводим выбранную строку', resultat.fetchall())
+        SMS=str(input('Выберите имя столбца  для корректировки или EXT-для выхода'))
+
+        if SMS=='EXT':
+            break
+        else:
+            SMS_info = str(input('На какое значение поменять'))
+            SMS_X=0
+            for i in names:
+                if SMS == i:
+                    print('вы выбрали имя столбца для коректировки\n', i, '\n', 'хотите поменять на следующее значение', SMS_info)
+                    print(id_info)
+                    cursor.execute(f'''UPDATE car SET {i} = ? WHERE id=?''', (SMS_info, id_info))
+                    # cursor.execute(f"""UPDATE car set {i} = {SMS_info} WHERE id={id_info}""")
+                    print('Выводим результат', resultat.fetchall())
+                    SMS_X+=1
+            if SMS_X==0: print('Повторите попытку')
+            #cursor.execute('UPDATE Users SET age = ?       WHERE username = ?',    (29, 'newuser'))
+            #cursor.execute('UPDATE users SET user_status=? WHERE id=?',            ('normal',id))
+        #INSERT INTO users (name, age) VALUES ('Tom', 22);
+        #обновление инфо в строке: UPDATE work SET person = "tae" WHERE person = "tay";
+        #UPDATE имя_таблицы SET имя_столбца = новое_значение WHERE условие
+        #"""Update sqlitedb_developers set salary = 10000 where id = 4"""
+
+    cursor.close()
--- sql CREATE INSERT.py
+++ sql CREATE INSERT.py
@@ -1,37 +1,34 @@
+from Procedure.proc import ADD_info, All_print_consol, ADD_basa, create_connection, ADD_colum, corect_info
 import sqlite3
 
-con=sqlite3.connect('sqlite3.db') #Создаем файл с базой данных
-cur = con.cursor()
+# con = sqlite3.connect('../sqlite3.db') #Создаем подключение к базе данных к файлу с наружи
+# cur = con.cursor()
+path = 'sqlite3.db'
 
-cur.execute ('''CREATE TABLE IF NOT EXISTS car (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        mode STRING,
-        model STRING,
-        name_id STRING) ''')
+connect = create_connection(path)
+# Строка определяет функцию create_connection(),
+# которая принимает путь к базе данных SQLite.
 
-# i = int
-# md = 'abc'
-# n= int
-# ml ='abc'
-x=0
-i = int(input('Введите сколько моделей хотите добавить в базу данных'))
-
-for x in range(i):
-    md = str(input('Укажите производителя'))
-    ml = str(input('Укажите модель'))
-    n  = str(input('Укажите серийный номер'))
-    print (f'''добавили {x+1}''')
-
-    x+=1
-    cur.execute(" INSERT INTO car (mode, model, name_id) VALUES(?,?,?);", (md, ml, n))
-    #
-    # В документации написано, что метод принимает два аргумента - строку запроса и кортеж аргументов
-    # cur.execute("INSERT INTO names VALUES(?, ?, ?);", (name, age, gender))
-
-res = cur.execute('SELECT * FROM car')                  #вывести в консоль все
-print('Вот что сейчас в базе данных', res.fetchall())
+#ADD_colum(connect, 'DATA_MODE') # добовление колонки
 
 
-con.commit()
-cur.close()
-con.close()
+print('для вывода всей информации - "ALL", для добавления в базу "ADD", для изменения информации в базе "CORE"')
+# sms = ''
+sms = str(input('Выберите вариант \n'))
+print(sms)
+if sms == "ADD":
+    ADD_info(connect)
+elif sms == "ALL":
+    All_print_consol(connect)
+elif sms == "CORE":
+    corect_info(connect)
+else:
+    print("Ввели не верный запрос")
+
+
+
+
+
+connect.commit()
+
+connect.close()
